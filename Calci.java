dc58b2f8a268beb16944be74af4511fb4b698be5
AravindVinayaga <aravindurai5@gmail.com>
2024-05-18 15:24:50 +0530

    implementation addes

--- Calci.java
+++ Calci.java
@@ -1,3 +1,10 @@
 Public Interface Calci{
     int add();
 }
+
+public class CalciImplements implements Calci{
+    public static void main(String[]args){
+        public int add(int a, int b){
+            System.out.println(a+b);
+        }
+    }
